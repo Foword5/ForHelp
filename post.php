583f038d6da4b8885832bf7ef64689d7341d42a0
Jules Delamare <julesd2509@gmail.com>
2021-12-26 20:35:26 +0100

    fin de la page post.php

--- post.php
+++ post.php
@@ -13,7 +13,7 @@
             $post = getPost($connexion,$postid);
             if (!$post) header("Location:unknow.html");
 
-
+            $autor = getUser($connexion,$post["userid"]);
             
         }else header("Location:unknow.html"); 
     ?>
@@ -34,7 +34,52 @@
             <p>
                 <?php echo $post["text"] ?>
             </p>
+            <table id="post_bot">
+                <tr>
+                    <td><?php echo $autor["username"]; ?></td>
+                    <td class="table_right"><a href="answer.php?post=<?php echo $postid;?>"><button>Écrire une réponse</button></a></td>
+                </tr>
+            </table>
         </div>
+
+        <?php
+            // ------Answers
+
+            $req="SELECT * from answers where postid = ? ORDER BY isgood DESC";
+
+            $stmt = mysqli_prepare($connexion, $req);
+            mysqli_stmt_bind_param($stmt, "i", $postid);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            mysqli_stmt_close($stmt);
+
+            foreach($result as $answer){
+                if($answer["isgood"] == 1){
+                    echo "
+                        <div class='answer'>
+                            <div class='answer_top'>
+                                <img src='data/img/check.png' alt='Good answer' class='check_img' title='Réponse validé par l'auteur'>
+                                ".getUser($connexion,$answer["userid"])["username"]."
+                            </div>
+                            <div class='answer_text'>
+                                ".$answer["text"]."
+                            </div>
+                        </div>
+                        ";
+                }else{
+                    echo "
+                        <div class='answer'>
+                            <div class='answer_top'>
+                                ".getUser($connexion,$answer["userid"])["username"]."
+                            </div>
+                            <div class='answer_text'>
+                                ".$answer["text"]."
+                            </div>
+                        </div>
+                        ";
+                }
+            }
+        ?>
     </body>
     <?php
         mysqli_close($connexion);
