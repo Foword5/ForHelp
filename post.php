9f8e584a3f89c328e5e3a1da9c6e1227b5d5cdf2
Jules Delamare <julesd2509@gmail.com>
2021-12-26 20:32:05 +0100

    ajouts du css pour les boutons

--- post.php
+++ post.php
@@ -10,18 +10,9 @@
             $connexion=mysqli_connect($host,$login,$mdp,$bdd)
             or die("connexion impossible");
 
-            $req="SELECT * from posts where postid = " . $postid;
-            
-            $result = mysqli_query($connexion,$req);
+            $post = getPost($connexion,$postid);
+            if (!$post) header("Location:unknow.html");
 
-            if ($result) {
-                if(mysqli_num_rows($result)>=1){
-                    foreach($result as $ligne) $post = $ligne;
-                    
-                    echo getCategoryArbo($connexion,$post["categoryid"]);
-
-                }else header("Location:unknow.html"); 
-            }else header("Location:unknow.html");
 
             
         }else header("Location:unknow.html"); 
@@ -30,8 +21,22 @@
         <meta charset="UTF-8">
         <title>ForHelp - <?php echo $post["title"]; ?></title>
         <link href="styles/style.css" rel="stylesheet"/>
+        <link href="styles/post.css" rel="stylesheet"/>
     </head>
     <body>
-        
+        <div id="post">
+            <div id="arbo">
+                <?php echo getCategoryArbo($connexion,$post["categoryid"]); ?>
+            </div>
+            <h3>
+                <?php echo $post["title"] ?>
+            </h3>
+            <p>
+                <?php echo $post["text"] ?>
+            </p>
+        </div>
     </body>
+    <?php
+        mysqli_close($connexion);
+    ?>
 </html>
