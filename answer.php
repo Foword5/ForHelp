3b7c67a6b6a3102ea0756f554010a9bd2610fcfe
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2021-12-30 03:44:58 +0100

    debut formulaire answer
    answer.php fonctionne comme post.php et nécessiste sendaswer.php pour l'envoie de la réponse
    newpost.php aura besoin d'un script similaire alors je l'ai créé

--- answer.php
+++ answer.php
@@ -6,7 +6,7 @@
         <link href="styles/style.css" rel="stylesheet"/>
     </head>
     <body>
-    <?php include 'navbar.php'; ?>
+        <?php include 'navbar.php'; ?>
         <?php
             if(isset($_GET["post"])){
                 require "data/db_login.php";
@@ -18,5 +18,39 @@
                 echo "Vous voulez répondre au post : " . getPost($connexion,$_GET["post"])["title"] ;
             }
         ?>
+
+        
+        <form method="POST" action="newanswer.php" class="post-form">
+            <!-- chemin d'accès -->
+            <a></a>
+            <br />
+            <!-- post d'origine -->
+            <!--
+            <div id="post">
+                <div id="arbo">
+                    <?php echo getCategoryArbo($connexion,$post["categoryid"]); ?>
+                </div>
+                <h3>
+                    <?php echo $post["title"] ?>
+                </h3>
+                <p>
+                    <?php echo $post["text"] ?>
+                </p>
+                <table id="post_bot">
+                    <tr>
+                        <td><?php echo $autor["username"]; ?></td>
+                        <td class="table_right"><a href="answer.php?post=<?php echo $postid;?>"><button>Écrire une réponse</button></a></td>
+                    </tr>
+                </table>
+            </div>
+            -->
+            <br />
+            <label><b>réponse</b></label>
+            <br />
+            <textarea class="proposition-textarea" name="description" required=""></textarea>
+            <br />
+            <button type="submit" class="proposition-bouton" name="submit">Poster</button>
+        </form>
+
     </body>
 </html>
