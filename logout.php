a8dd6e9b7fe4b26e965392c9339e0dbeab109a4b
Jules Delamare <julesd2509@gmail.com>
2022-01-02 23:03:27 +0100

    fixed logout

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
     if(session_status() != PHP_SESSION_ACTIVE){
         session_start();
     }
-    if (isset($_POST['deconnexion']) && $_POST['deconnexion']=="logout"){
+    if (isset($_SESSION["connected"])){
         session_destroy();
         unset($_SESSION['connected']);
         $_SESSION['connected']= NULL;
