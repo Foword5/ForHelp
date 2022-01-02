e0be715bfbe7e697ce78f25b0af13383f0c5daf3
Jules Delamare <julesd2509@gmail.com>
2022-01-02 23:28:05 +0100

    account bateau pour test

--- account.php
+++ account.php
@@ -6,9 +6,25 @@
         <link href="styles/style.css" rel="stylesheet"/>
     </head>
     <body>
-        <?php include "data/navbar.php"; ?>
+        <?php include "data/navbar.php"; 
+            include "data/functions.php";
+            include "data/db_login.php"?>
         <main>
-            
+            <?php
+                if(session_status() != PHP_SESSION_ACTIVE){
+                    session_start();
+                }
+                $connexion=mysqli_connect($host,$login,$mdp,$bdd) or die("connexion impossible");
+                if(!(isset($_GET["user"]))){
+                    header('Location: unknow.html');
+                }else{
+                    if(is_numeric($_GET["user"]) && $_GET["user"]!=$_SESSION["connected"]){
+                        echo getUser($connexion,$_GET["user"])["username"];
+                    }else{
+                        echo "Mon compte : ".getUser($connexion,$_GET["user"])["username"];
+                    }
+                }
+            ?>
         </main>
     </body>
 </html>
