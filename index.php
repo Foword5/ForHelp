34abf9c7bcac581ac9e16e6bb0c10820ad68df47
Jules Delamare <julesd2509@gmail.com>
2022-01-02 17:30:17 +0100

    fusion des différentes branches

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <meta charset="UTF-8">
-        <title>Rechercher - Forum d'entraide</title>
-        <link href="styles/style.css" rel="stylesheet"/>
+            <meta charset="UTF-8">
+            <title>Rechercher - ForHelp</title>
+            <link href="styles/style.css" rel="stylesheet"/>
+            <link href="styles/index.css" rel="stylesheet"/>
     </head>
     <body>
         <?php
         include 'navbar.php';
         include 'data/functions.php';
-
-        $login = "root";
-        $mdp = "";
-        $host = "localhost";
-        $bdd="projet";
+        include 'data/db_login.php';
 
         $connexion=mysqli_connect($host,$login,$mdp,$bdd) or die("connexion impossible");
         $bd_get = "SELECT * FROM posts";
@@ -22,14 +19,15 @@
 
 
         while($ligne = mysqli_fetch_array($result, MYSQLI_ASSOC)){    
-            echo "<table border='1'><hr>";
-            echo "<h2>" .getCategoryArbo($connexion,$ligne['categoryid']). "</h2>";
-            echo "<h3>".$ligne['title']."</h3>";
-            echo $ligne['text'];
-            echo "</table>";
+            echo " <div id='index'>
+                        <b id='arbo'>" .getCategoryArbo($connexion, $ligne['categoryid']). "</b>
+                        <h3>" .$ligne['title']. "</h3>
+                        <p>" .$ligne['text']. "</p>
+                        </br>
+                        <a href='post.php?id=".$ligne["postid"]."'><button id='voirplus'>Voir plus</button></a>
+                    </div>
+            ";
         }
-
-        echo "</table>";
 
         ?>
     </body>
