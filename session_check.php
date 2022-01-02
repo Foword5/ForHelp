e6e2b2dad15ca89cbc9d723dda4edf0f5764d2cf
Jules Delamare <julesd2509@gmail.com>
2022-01-02 22:45:52 +0100

    verif session

--- session_check.php
+++ session_check.php
@@ -1,5 +1,7 @@
 <?php
-    session_start();
+    if(session_status() != PHP_SESSION_ACTIVE){
+        session_start();
+    }
     require "data/db_login.php";
 
     if(isset($_SESSION['connected'])){
