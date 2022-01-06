2689c5588c95af217ceb78ce6b7a6ad9de92ba64
Jules Delamare <julesd2509@gmail.com>
2022-01-06 01:27:39 +0100

    fix pb

--- sendpost.php
+++ sendpost.php
@@ -6,7 +6,7 @@
         $connexion=mysqli_connect($host,$login,$mdp,$bdd)
         or die("connexion impossible");
 
-        foreach ($_POST as $k => $v) $$k = $v;
+        foreach ($_POST as $k => $v) $$k = htmlspecialchars($v);
 
         $req="SELECT * from categories where categoryid = ?";
 
@@ -25,11 +25,15 @@
 
         $req="INSERT INTO posts(title,text,categoryid,userid) VALUES (?,?,?,?)";
 
-        session_start();
+        if(session_status() != PHP_SESSION_ACTIVE){
+            session_start();
+        }
         $stmt = mysqli_prepare($connexion, $req);
         mysqli_stmt_bind_param($stmt, "ssii",$title,$text,$categoryid,$_SESSION["connected"]);
         mysqli_stmt_execute($stmt);
         mysqli_stmt_close($stmt);
+        header('Location: index.php');
+        exit(0);
     }
     header('Location: writepost.php');
 ?>
